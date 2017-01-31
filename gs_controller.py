6924a8f258e79206a5481fad0fb923c20d5b31a5
Alex I. Ramirez <alex@arcompware.com>
2017-01-30 23:20:44 -0800

    UI Update

--- gs_controller.py
+++ gs_controller.py
@@ -8,7 +8,7 @@ BTN2 = 40
 LED2 = 36
 
 class GameshowController:
-    def __init__(self, team1="Smith", team2="Lopez"):
+    def __init__(self, team1="Team 1", team2="Team 2"):
         self._game = Gameshow()
         self._team1 = team1
         self._team2 = team2
@@ -31,8 +31,9 @@ class GameshowController:
     def get_score(self):
         return (self._game.get_player1_score(), self._game.get_player2_score())
     def set_score_view(self, team_sel = -1):
+        #print(team_sel)
         if team_sel == 0:
-            self._view.set_score(">"+self._team1+ "< "+str(self.get_score()) +" " +self._+team2)
+            self._view.set_score(">"+self._team1+ "< "+str(self.get_score()) +" " +self._team2)
         elif team_sel == 1:
             self._view.set_score(self._team1+ " "+ str(self.get_score()) +">" +self._team2+"<")
         else:
@@ -43,10 +44,11 @@ class GameshowController:
         self._view.btn_disable(self._view._btn_right)
         question = self._game.next_question()
         self._view.set_question(question[0])
+        self._view.set_answer(question[1])
+        self._view.update()
         self._player = self._buzzer.play()
         if self._player != None:
-            self.set_score_view(self.Player)
-        self._view.set_answer(question[1])
+            self.set_score_view(self._player)
         self._view.btn_enable(self._view._btn_reveal)
 
     def reveal_question(self):
